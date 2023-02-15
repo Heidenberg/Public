98eb9f31decbceff67f2a7d095a483f63564815c
Heidenberg <123549762+Heidenberg@users.noreply.github.com>
2023-02-15 16:45:23 +0100

    production script
    Added date

--- csv_script.sh
+++ csv_script.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-FILE=csv_log.csv
+FILE=/mnt/c/Users/david/Repo/konfig-final-itinf21-team-burger/csv_log.csv
+DATE=$(date +"%Y-%m-%d %H:%M:%S")
 
-printf "Memory\t\tDisk\t\tCPU\n"
+if [ ! -f $FILE ]; then
+printf "Memory;Disk;CPU;Date\n" > $FILE
+fi
 end=$((SECONDS+1))
 while [ $SECONDS -lt $end ]; do
-MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }');
-DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}');
-CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}');
-echo "$MEMORY$DISK$CPU" > $FILE
+MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }' | tr -d ' \t\r\n')
+DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}' | tr -d ' \t\r\n')
+CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}' | tr -d ' \t\r\n')
+echo "$MEMORY;$DISK;$CPU;$DATE" >> $FILE
 sleep 1
+done
